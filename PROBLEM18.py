30657c9812f722e214ea88424be3bf477d98f53f
Ashutosh Shukla <aashutoshukla12@gmail.com>
2026-03-31 23:45:31 +0530

    Create PROBLEM18.py

---
+++ PROBLEM18.py
@@ -0,0 +1,10 @@
+# We will write a program that receives a positive integer from the user
+# and tells whether it is an odd or even number.
+# Complete the appropriate pseudo code for the underlined part.
+
+num = int(input("Enter a positive integer: "))
+
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
