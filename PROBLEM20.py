8c177ab80fbc9f9b772ee864b5b18e7eeccd0816
Ashutosh Shukla <aashutoshukla12@gmail.com>
2026-04-01 23:51:27 +0530

    Create PROBLEM20.py

---
+++ PROBLEM20.py
@@ -0,0 +1,9 @@
+#Write and save the following name and address in the name and address variables, respectively. Write a code that 
+#prints them to the screen
+# Store name and address in variables
+name = "Ashutosh Shukla"
+address = "Shiamgir, Delhi, India"
+
+# Print the values
+print("Name:", name)
+print("Address:", address)
